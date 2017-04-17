e8ecc6fdda36e1b12474928ff154d03811ead636
Chris Pacia <ctpacia@gmail.com>
2017-04-16 20:57:19 -0400

    Hook up travis

---
+++ test_compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+pwd
+go test -coverprofile=api.cover.out ./
+echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
+awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
+rm -rf *.cover.out
