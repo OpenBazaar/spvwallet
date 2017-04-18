be1c250ec300609e0e248b08609a8a8a68f6d680
Chris Pacia <ctpacia@gmail.com>
2017-04-17 22:12:13 -0400

    Update README.md

--- examples/client.go
+++ examples/client.go
@@ -6,11 +6,8 @@ import (
 	"github.com/OpenBazaar/spvwallet/db"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/op/go-logging"
+	"os"
 	"sync"
-)
-
-var stdoutLogFormat = logging.MustStringFormatter(
-	`%{color:reset}%{color}%{time:15:04:05.000} [%{shortfunc}] [%{level}] %{message}`,
 )
 
 func main() {
@@ -19,6 +16,12 @@ func main() {
 
 	// Create a new config
 	config := spvwallet.NewDefaultConfig()
+
+	// Make the logging a little prettier
+	backend := logging.NewLogBackend(os.Stdout, "", 0)
+	formatter := logging.MustStringFormatter(`%{color:reset}%{color}%{time:15:04:05.000} [%{shortfunc}] [%{level}] %{message}`)
+	stdoutFormatter := logging.NewBackendFormatter(backend, formatter)
+	config.Logger = logging.MultiLogger(stdoutFormatter)
 
 	// Use testnet
 	config.Params = &chaincfg.TestNet3Params
